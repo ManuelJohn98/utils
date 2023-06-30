862865ae88be5c815f9301d458349d23969ce00f
ManuelJohn98 <majo00004@stud.uni-saarland.de>
2023-06-30 17:06:51 +0200

    handle permission error

--- downloadCleanup.py
+++ downloadCleanup.py
@@ -25,20 +25,23 @@ def cleanup():
         # Get the extension of the file
         extension = file.split(".")[-1]
         # Check if the extension is known
-        if os.path.exists("C:\\Users\\{}\\Downloads\\{}".format(os.getlogin(), extension)):
-            # Move the file into the respective folder
-            os.rename("C:\\Users\\{}\\Downloads\\{}".format(os.getlogin(), file), "C:\\Users\\{}\\Downloads\\{}\\{}".format(os.getlogin(), extension, file))
-        else:
-            # Create a new folder for the extension
-            os.mkdir("C:\\Users\\{}\\Downloads\\{}".format(os.getlogin(), extension))
-            logging.info("Created new folder for extension {}.".format(extension))
-            # Move the file into the respective folder and catch the exception if the file already exists
-            try:
+        try:
+            if os.path.exists("C:\\Users\\{}\\Downloads\\{}".format(os.getlogin(), extension)):
+                # Move the file into the respective folder
                 os.rename("C:\\Users\\{}\\Downloads\\{}".format(os.getlogin(), file), "C:\\Users\\{}\\Downloads\\{}\\{}".format(os.getlogin(), extension, file))
-            except FileExistsError:
-                conflicting_target_files = [target_file if file in target_file else ... for target_file in os.listdir("C:\\Users\\{}\\Downloads\\{}".format(os.getlogin(), extension))]
-                # Rename the file to avoid overwriting
-                os.rename("C:\\Users\\{}\\Downloads\\{}".format(os.getlogin(), file), "C:\\Users\\{}\\Downloads\\{}\\{}-{}".format(os.getlogin(), extension, len(conflicting_target_files), file))
+            else:
+                # Create a new folder for the extension
+                os.mkdir("C:\\Users\\{}\\Downloads\\{}".format(os.getlogin(), extension))
+                logging.info("Created new folder for extension {}.".format(extension))
+                # Move the file into the respective folder and catch the exception if the file already exists
+                try:
+                    os.rename("C:\\Users\\{}\\Downloads\\{}".format(os.getlogin(), file), "C:\\Users\\{}\\Downloads\\{}\\{}".format(os.getlogin(), extension, file))
+                except FileExistsError:
+                    conflicting_target_files = [target_file if file in target_file else ... for target_file in os.listdir("C:\\Users\\{}\\Downloads\\{}".format(os.getlogin(), extension))]
+                    # Rename the file to avoid overwriting
+                    os.rename("C:\\Users\\{}\\Downloads\\{}".format(os.getlogin(), file), "C:\\Users\\{}\\Downloads\\{}\\{}-{}".format(os.getlogin(), extension, len(conflicting_target_files), file))
+        except PermissionError:
+            logging.warn(f"PermissionError with file {file}")
     logging.info("Moved {} files.".format(files_moved))
 
 
